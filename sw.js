885b850c03f0ea85da35be8319b4799a6165ed1b
lipenghuihenu <493284461@qq.com>
2021-05-23 23:17:46 +0800

    Site updated: 2021-05-23 23:17:46

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","789ccaa079d2be8a739592739c0df994"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
